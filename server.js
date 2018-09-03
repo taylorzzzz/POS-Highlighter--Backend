25a154e670c66565710f09067c601a2a2a45ca8a
Taylor Ward <taylorward@Taylors-MacBook-Pro.local>
2018-09-03 12:12:38 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const Express = require('express');
+const bodyParser = require('body-parser');
+const tokenizer = require('wink-tokenizer');
+const posTagger = require( 'wink-pos-tagger' );
+ 
+const PORT = process.env.PORT || 3001;
+
+const app = Express();
+const myTokenizer = tokenizer();
+const tagger = posTagger();
+
+app.use(bodyParser.json());
+
+app.post('/api', (req, res) => {
+
+  const text = req.body.text;
+
+  const taggedText = tagger.tagSentence(text);
+
+  res.json(taggedText);
+})
+
+
+
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+
+  console.log(`now listening on port ${PORT}`);
+})
