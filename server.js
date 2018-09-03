d09c3666d2c36035372a2110e9510fffdda2e9ac
Taylor Ward <taylorward@Taylors-MacBook-Pro.local>
2018-09-03 12:39:40 -0700

    Allow CORS

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const tagger = posTagger();
 
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
 app.post('/api', (req, res) => {
 
   const text = req.body.text;
